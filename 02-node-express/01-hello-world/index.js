42c9f4ef839a72617a1dbfa782713a6fcb83bdc6
GabrielyFarias <132363640+GabrielyFarias@users.noreply.github.com>
2023-10-20 13:31:25 -0400

    aula 19/10

---
+++ 02-node-express/01-hello-world/index.js
@@ -0,0 +1,12 @@
+const { createServer } = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+const server = createServer((req, res) => {
+res.statusCode = 200
+res.setHeader('Content-Type', 'text/plain')
+res.write('Hello World!');
+res.end();
+})
+server.listen(port, hostname, () => {
+console.log(`Servidor executando em http://${hostname}:${port} 🚀`);
+});
